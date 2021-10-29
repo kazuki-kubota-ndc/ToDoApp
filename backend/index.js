898f7fc1b296eda6ad6e4df42e281456ca057343
kazuki <kazuryahuka@outlook.jp>
2021-10-29 16:54:08 +0900

    ver1.0.0

--- backend/index.js
+++ backend/index.js
@@ -9,19 +9,176 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
+/* 画面初期表示時の処理 */
 app.get("/api", (req, res) => {
+  res.header("Content-Type", "application/json; charset=utf-8");
   db.pool.connect((err, client) => {
     if (err) {
       console.log(err);
     } else {
-      client.query('SELECT NAME FROM TO_DO_LIST_DATA', (err, result) => {
-        res.json({ message: result.rows[0].name });
+      client.query('SELECT NO,NAME,CHECK_FLG,SORT FROM TO_DO_LIST_HED ORDER BY SORT', (err, result) => {
+        if(err) {
+          console.log(err.stack);
+          res.json({ result: 0 });
+        }else {
+          if(result.rows.length!=0) {
+            res.json({
+              result: 1 ,
+              data: result.rows
+            });
+          /* 検索結果0件 */
+          }else {
+            res.json({ result: 2 });
+          }
+        }
       });
     }
   });
-})
+});
+
+app.get("/delete", (req, res) => {
+  res.header("Content-Type", "application/json; charset=utf-8");
+  console.log('delete[no:'+req.query.no+']');
+
+  db.pool.connect((err, client) => {
+    var err_flg = false;
+    if (err) {
+      console.log(err);
+      res.json({ result: 0 });
+    } else {
+      /* データを削除 */
+      const delete_query = {
+        text: 'DELETE FROM TO_DO_LIST_HED WHERE NO = $1',
+        values: [req.query.no],
+      }
+      client.query(delete_query, (err, result) => {
+        if(err) {
+          console.log(err.stack);
+          res.json({ result: 0 });
+        }else {
+          res.json({ result: 1 });
+        }
+      });
+    }
+  });
+});
+
+app.get("/test", (req, res) => {
+  res.header("Content-Type", "application/json; charset=utf-8");
+  if(req.query.no==6) {
+    res.json({ result: 1 });
+  }else {
+    res.json({ result: 0 });
+  }
+});
+
+/* チェックボックスをチェックした時の処理 */
+app.get("/update_check", (req, res) => {
+  res.header("Content-Type", "application/json; charset=utf-8");
+  console.log('update_check[no:'+req.query.no+', check_flg:'+req.query.check_flg+']');
+
+  function return_result(flg) {
+    /* 正常処理：１、エラー：０を返す */
+    if(flg) {
+      res.json({ result: 0 });
+    }else {
+      res.json({ result: 1 });
+    }
+  }
+
+  db.pool.connect((err, client) => {
+    var err_flg = false;
+    if (err) {
+      console.log(err);
+      err_flg = true;
+    } else {
+      /* CHECK_FLGをアップデート */
+      const update_query = {
+        text: 'UPDATE TO_DO_LIST_HED SET CHECK_FLG = $2 WHERE NO = $1',
+        values: [req.query.no, req.query.check_flg],
+      }
+      client.query(update_query, (err, result) => {
+        if(err) {
+          console.log(err.stack);
+          err_flg = true;
+        }else {
+          /* 更新件数0件 */
+          if(result.rowCount==0) {
+            err_flg = true;
+          }
+          return_result(err_flg);
+        }
+      });
+    }
+  });
+});
+
+/* inputでEnterを押した時の処理 */
+app.get("/insert", (req, res) => {
+  res.header("Content-Type", "application/json; charset=utf-8");
+  console.log('insert[name:'+req.query.name+']');
+
+  function insert_hed(no, name, sort){
+    db.pool.connect((err, client) => {
+      if (err) {
+        console.log(err);
+      }else {
+        var err_flg = false;
+        /* DTLを削除 */
+        if(!err_flg) {
+          const delete_query = {
+            text: 'DELETE FROM TO_DO_LIST_DTL WHERE NO = $1',
+            values: [no],
+          }
+          client.query(delete_query, (err, result) => {
+            if(err) {
+              console.log(err.stack);
+              err_flg = true;
+            }
+          });
+        }
+        /* 登録 */
+        if(!err_flg) {
+          const insert_query = {
+            text: 'INSERT INTO TO_DO_LIST_HED (NO, NAME, CHECK_FLG, SORT) VALUES ($1, $2, $3, $4)',
+            values: [no, name, 0, sort],
+          }
+          client.query(insert_query, (err, result) => {
+            if(err) {
+              console.log(err.stack);
+              res.json({ result: 0 });
+            }else {
+              res.json({
+                result: 1,
+                key: no
+              });
+            }
+          });
+        }else {
+          res.json({ result: 0 });
+        }
+      }
+    });
+  }
+
+  db.pool.connect((err, client) => {
+    if (err) {
+      console.log(err);
+      res.json({ result: 0 });
+    } else {
+      /* 登録するNO,SORTを取得 */
+      client.query('SELECT MAX(NO) AS MAX_NO, MAX(SORT) AS MAX_SORT FROM TO_DO_LIST_HED', (err, result) => {
+        if(err) {
+          console.log(err.stack);
+        }else {
+          insert_hed(result.rows[0].max_no + 1, req.query.name, result.rows[0].max_sort + 1);
+        }
+      });
+    }
+  });
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'../frontend/build/index.html'));
@@ -29,4 +186,4 @@ app.get('*', (req, res) => {
 
 app.listen(port, () => {
   console.log(`listening on *:${port}`);
-})
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,13 @@ import 'bulma/css/bulma.min.css';
 
 const getKey = () => Math.random().toString(32).substring(2);
 
-function Todo() {
+/* リスト表示 */
+function Todo({ array }) {
+
   const [items, setItems] = React.useState([]);
   const [filter, setFilter] = React.useState('ALL');
 
+  /* チェックボックスをチェックした時の処理 */
   const handleCheck = checked => {
     const newItems = items.map(item => {
       if (item.key === checked.key) {
@@ -17,69 +20,188 @@ function Todo() {
       }
       return item;
     });
+    var check_flg = 0;
+    if(checked.done) {
+      check_flg = 1;
+    }
     setItems(newItems);
+    fetch('/update_check?no='+checked.key+'&check_flg='+check_flg)
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          /* 更新失敗時 */
+          if(!data.result==1) {
+            alert('update failed');
+            window.location.reload();
+          }
+        }
+      );
   };
 
+  /* テスト */
+  const test = no => {
+alert(no);
+  }
+
+  /* 削除処理 */
+  const rowDelete = no => {
+    function del_item(target) {
+      var i = -1;
+      var index = -1;
+      /* 削除する行のindexを取得 */
+      items.map(item => {
+        i++;
+        if (item.key === no) {
+          index = i;
+        }
+      });
+      /* newItemsのindex行を削除してsetItemsする(itemsをspliceしてsetItemsするのはダメ) */
+      const newItems = [...items];
+      newItems.splice(index,1);
+      setItems(newItems);
+    }
+
+    fetch('/delete?no='+no)
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          if(data.result==1) {
+            del_item(no);
+          }else {
+            alert('delete failed');
+          }
+        }
+      );
+  }
+
+  /* inputタブでEnterを押した時の処理 */
   const handleAdd = text => {
-    setItems([...items, { key: getKey(), text, done: false }]);
+    fetch('/insert?name='+text)
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          if(data.result==1) {
+            setItems([...items, { key: data.key, text, done: false }]);
+          }else {
+            alert('insert failed');
+          }
+        }
+      );
   };
 
+  /* フィルターを変えた時の処理 */
   const handleFilterChange = value => setFilter(value);
 
+  /* リスト表示のフィルター処理 */
   const displayItems = items.filter(item => {
     if (filter === 'ALL') return true;
     if (filter === 'TODO') return !item.done;
     if (filter === 'DONE') return item.done;
   });
 
+  /* DBから取得したデータを初期データとしてセット */
+  useEffect(() => {
+    const defaultItems = array.map(item => {
+      item.text = item.name;
+      item.key = item.no;
+      var done_flg = false;
+      if(item.check_flg==1){
+        done_flg = true;
+      }
+      item.done = done_flg;
+      return item;
+    });
+    /* DBからのデータ取得前はリストを表示しない */
+    if(array[0].key) {
+      setItems(defaultItems);
+    }
+  }, [array]);
+
   return (
     <div className="panel">
       <div className="panel-heading">
          ToDo List
       </div>
+      {/* テキスト入力欄 */}
       <Input onAdd={handleAdd} />
+      {/* フィルター */}
       <Filter
         onChange={handleFilterChange}
         value={filter}
       />
+      {/* リスト表示部分 */}
       {displayItems.map(item => (
         <TodoItem
-          key={item.text}
+          key={item.key}
           item={item}
           onCheck={handleCheck}
+          onClickDel={rowDelete}
+          modalTest={test}
          />
       ))}
+      {/* フッター部分 */}
       <div className="panel-block">
-        {items.length} items
+        全 {items.length} 件
       </div>
     </div>
+
   );
 }
 
-function TodoItem({ item, onCheck }) {
+/* リスト中身 */
+function TodoItem({ item, onCheck, onClickDel, modalTest }) {
 
   const handleChange = () => {
     onCheck(item);
   };
 
+  const row_delete = () => {
+    onClickDel(item.key);
+  }
+
+  const style = {
+    padding: 0,
+    margin: 0,
+  };
+
+  const style2 = {
+    padding: 5,
+    margin: 0,
+  }
+
+  const modal_test = () => {
+    modalTest(item.key);
+  }
+
   return (
-    <label className="panel-block">
-      <input
-        type="checkbox"
-        checked={item.done}
-        onChange={handleChange}
-      />
-      <span
-        className={classNames({
-          'has-text-grey-light': item.done
-        })}
-      >
-        {item.text}
-      </span>
+    <label class="columns" className="panel-block" style={ style }>
+        <div class="column is-11" style={ style2 }>
+          <input
+            type="checkbox"
+            checked={item.done}
+            onChange={handleChange}
+          />
+          <span
+            className={classNames({
+              'has-text-grey-light': item.done
+            })}
+          >
+            {item.text}
+          </span>
+        </div>
+{/*        <div class="column is-1" style={ style2 }>
+          <button onClick={modal_test}>テスト</button>
+        </div>
+*/}
+        <div class="column is-1" style={ style2 }>
+          <button onClick={row_delete}>削除</button>
+        </div>
     </label>
+
   );
 }
 
+/* inputタグ */
 function Input({ onAdd }) {
   const [text, setText] = React.useState('');
 
@@ -106,6 +228,7 @@ function Input({ onAdd }) {
   );
 }
 
+/* フィルター */
 function Filter({ value, onChange }) {
   const handleClick = (key, e) => {
     e.preventDefault();
@@ -133,19 +256,25 @@ function Filter({ value, onChange }) {
   );
 }
 
+/* 初期処理 */
 function App() {
+  const [array, setArray] = useState([{}]);
 
-  const [message, setMessage] = useState('');
   useEffect(() =>{
     fetch('/api')
       .then((res) => res.json())
-      .then((data) => setMessage(data.message));
+      .then(
+        (data) => {
+          if(data.result==1) {
+            setArray(data.data);
+          }
+        }
+      );
   },[])
 
   return (
     <div className="container is-fluid">
-      <Todo />
-      <p>{ message }</p>
+      <Todo array={ array }/>
     </div>
   );
 }
