daf9b6479afdb12e248502cca58728fd5ab0b406
kazuki <kazuryahuka@outlook.jp>
2021-11-19 11:55:13 +0900

    20211119-05

--- db/db.js
+++ db/db.js
@@ -3,6 +3,7 @@ require('pg').defaults.ssl = true;
 require('dotenv').config();
 
 exports.pool = new pg.Pool ({
+console.log('pool');
   host: process.env.ENV_HOST,
   database: process.env.ENV_DB,
   user: process.env.ENV_USER,
