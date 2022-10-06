aecf24bf4ef154d60eab8c42b2a1ac78a098f84d
kubota <kubota@ndatacom.co.jp>
2022-10-06 14:13:18 +0900

    addArrorMessage-change

--- frontend/src/AddUserModal.js
+++ frontend/src/AddUserModal.js
@@ -62,11 +62,11 @@ const AddUserModal = ({showModal, modalOpen, modalClose, modalData, setModalData
   /* 追加ボタン実行 */
   const onClickAddUser = () => {
     if(!addUserData.login_id) {
-      alert("ユーザーIDが入力されていません");
+      alert("ユーザーIDを入力して下さい");
     }else if(!addUserData.user_name) {
-      alert("ユーザー名が入力されていません");
+      alert("ユーザー名を入力して下さい");
     }else if(!addUserData.pass) {
-      alert("パスワードが入力されていません");
+      alert("パスワードを入力して下さい");
     }else {
 
       /* 新規作成時、権限は「ユーザー」固定 */
